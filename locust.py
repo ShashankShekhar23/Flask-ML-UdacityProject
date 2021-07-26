ce52f49ae4561f4cc6da276e803c68d6ac6b33bd
ShashankShekhar23 <86247520+ShashankShekhar23@users.noreply.github.com>
2021-07-26 18:22:02 +0530

    Update locust.py

--- locust.py
+++ locust.py
@@ -1,1 +1,14 @@
+from locust import HttpLocust, TaskSet, task
+
+class MyTaskSer(TaskSet):
+	@task
+	def get_user_list(self):
+		self.client.get("/api/users")
+
+	@task
+	def get_user_list(self):
+		self.client.get("/api/users/2")
+
+class MyLocust(HttpLocust):
+	task_set = MyTaskSet
 
