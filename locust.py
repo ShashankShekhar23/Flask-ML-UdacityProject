c54bda72f17aed2109162918e8e86da1f1f90ce0
ShashankShekhar23 <86247520+ShashankShekhar23@users.noreply.github.com>
2021-07-27 23:25:47 +0530

    Update locust.py

--- locust.py
+++ locust.py
@@ -6,10 +6,11 @@ class MyTaskSet(TaskSet):
 		self.client.get("/api/users")
 
 	@task
-	def get_user_list(self):
-		self.client.get("/api/users/2")
+	def post_user_list(self):
+		self.client.post("/api/users/2")
 
 class MyLocust(HttpUser):
 	task_set = MyTaskSet
-
-
+	min_wait = 5000
+	max_wait = 10000
+	host = "https://flaskmludacityproject.azurewebsites.net"
